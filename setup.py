f4ce267dc5fa920aa9ec19111d56d52140c8e488
Lexa <Lexa.B@outlook.com>
2025-02-27 15:36:18 -0800

    setting up for pip installation

--- setup.py
+++ setup.py
@@ -10,7 +10,14 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/Lexa-B/AyakaUtils',
     packages=find_packages(),       # automatically find packages (will pick up ayaka_utils)
-    install_requires=[],            # list runtime dependencies if any
+    install_requires=[
+        "langchain==0.3.17", # For LangChain
+        "langchain_nvidia_ai_endpoints==0.3.9", # For NVIDIA AI Endpoints
+        "langchain_openai==0.3.4", # For OpenAI
+        "dotenv==0.9.9", # For loading environment variables
+        "hjson==3.1.0", # Even though this is for hjson, we use it for JSONC files, such as the model config
+        "rich==13.9.4", # For pretty printing
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
